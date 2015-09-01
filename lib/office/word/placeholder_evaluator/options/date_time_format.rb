b4c4129488b6c4a4861ce53638723bc35341b4ac
Matthew Hirst <matt@devicemagic.com>
2015-09-01 11:04:43 +0200

    Prevent error when no date is present.

--- lib/office/word/placeholder_evaluator/options/date_time_format.rb
+++ lib/office/word/placeholder_evaluator/options/date_time_format.rb
@@ -2,8 +2,11 @@ module Word
   module Options
     class DateTimeFormat < Word::Option
       def apply_option
-        date_time = DateTime.parse(placeholder.field_value.to_s)
-        placeholder.field_value = date_time.strftime(params)
+        value = placeholder.field_value
+        unless value.nil?
+          date_time = DateTime.parse(value.to_s)
+          placeholder.field_value = date_time.strftime(params)
+        end
       end
     end
   end
