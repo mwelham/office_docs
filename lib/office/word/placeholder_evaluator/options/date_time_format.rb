b05a10d7b03312ae6be3fe956f88ff1627eb4400
Matthew Hirst <matt@devicemagic.com>
2015-09-01 11:50:39 +0200

    Stop blank dates breaking placeholder option.

--- lib/office/word/placeholder_evaluator/options/date_time_format.rb
+++ lib/office/word/placeholder_evaluator/options/date_time_format.rb
@@ -3,7 +3,7 @@ module Word
     class DateTimeFormat < Word::Option
       def apply_option
         value = placeholder.field_value
-        unless value.nil?
+        if !value.nil? && value.length > 0
           date_time = DateTime.parse(value.to_s)
           placeholder.field_value = date_time.strftime(params)
         end
