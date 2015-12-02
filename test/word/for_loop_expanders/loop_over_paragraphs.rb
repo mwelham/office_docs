942c6ebb27825452e081b97c6d845effd430bd8d
Matthew Hirst <matt@devicemagic.com>
2015-12-02 17:45:22 +0200

    Added complex for loop test.

--- test/word/for_loop_expanders/loop_over_paragraphs.rb
+++ test/word/for_loop_expanders/loop_over_paragraphs.rb
@@ -5,6 +5,8 @@ module LoopOverParagraphsTest
   LOOP_OVER_GRAPHIC = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'loop_over_graphic_test.docx')
   LOOP_OVER_LIST = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'loop_over_list_test.docx')
 
+  COMPLEX_FOR_LOOP = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'complex_for_loop_word_template.docx')
+
   #DIFFERENT PARAGRAPH LOOPS
   #
   #
@@ -198,6 +200,44 @@ module LoopOverParagraphsTest
     assert File.stat(filename).size > 0
 
     correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'loop_over_list_test.docx'))
+    our_render = Office::WordDocument.new(filename)
+
+    assert docs_are_equivalent?(correct, our_render)
+
+    File.delete(filename)
+  end
+
+  def test_complex_loop
+    file = File.new('complex_for_loop_word_template.docx', 'w')
+    file.close
+    filename = file.path
+
+    doc = Office::WordDocument.new(COMPLEX_FOR_LOOP)
+    template = Word::Template.new(doc)
+    template.render(
+      {'fields' => {"Inspector_Name"=>"Dr Matt",
+       "Sites"=>
+        [{"Name"=>"Site A",
+          "Number_of_cool_things_at_the_site"=>"42",
+          "Checks"=>
+           [{"Check"=>"First Option", "Tools_found"=>[{"Tool_Name"=>"Bob"}, {"Tool_Name"=>"Hammer"}]},
+            {"Check"=>"2", "Tools_found"=>[{"Tool_Name"=>"Shovel"}]},
+            {"Check"=>"3"},
+            {"Check"=>"4", "Tools_found"=>[{"Tool_Name"=>"A"}, {"Tool_Name"=>"B"}, {"Tool_Name"=>"C"}, {"Tool_Name"=>"D"}]}],
+          "Employees"=>
+           [{"Name"=>"Pop", "Rating"=>"Good"},
+            {"Name"=>"Cap", "Rating"=>"Average"}]},
+         {"Name"=>"Site B",
+          "Number_of_cool_things_at_the_site"=>"2",
+          "Checks"=>[{"Check"=>"6", "Tools_found"=>[{"Tool_Name"=>"Pew"}, {"Tool_Name"=>"Shoe"}]}]}]
+          }
+        },{do_not_render: true})
+    template.word_document.save(filename)
+
+    assert File.file?(filename)
+    assert File.stat(filename).size > 0
+
+    correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'complex_for_loop_word_template.docx'))
     our_render = Office::WordDocument.new(filename)
 
     assert docs_are_equivalent?(correct, our_render)
