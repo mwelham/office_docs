cc4777170eea07c8f81f85d0eb86eba0704e5ffa
John Anderson <panic@semiosix.com>
2020-11-02 18:18:22 -0500

    allow running failed specs only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec'
 RSpec.configure do |config|
   # enable should syntax
   config.expect_with(:rspec) { |c| c.syntax = :should }
+  config.example_status_persistence_file_path = "spec/examples.txt"
 end
 
 # implement minimal minitest support for migrated tests
