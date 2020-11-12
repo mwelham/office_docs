14384b9d7ae8879332221f0ca71244bf26ad03dd
John Anderson <panic@semiosix.com>
2020-11-12 13:04:47 -0500

    place to hang performance benchmarks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,12 @@ require 'rspec'
 RSpec.configure do |config|
   # enable should syntax
   config.expect_with(:rspec) { |c| c.syntax = :should }
+
+  # allow for --only-failures
   config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # exclude things marked as performance benchmarks
+  config.filter_run_excluding performance: true
 end
 
 # implement minimal minitest support for migrated tests
