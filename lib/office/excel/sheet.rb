159d8e003993b257133e18af32e4d20cf487e349
John Anderson <panic@semiosix.com>
2020-11-24 11:43:57 -0500

    move SheetData to own file

--- lib/office/excel/sheet.rb
+++ lib/office/excel/sheet.rb
@@ -10,6 +10,7 @@ require_relative 'lazy_cell'
 require_relative 'location'
 require_relative 'range'
 require_relative 'excel_workbook'
+require_relative 'sheet_data'
 
 module Office
   # The design rationale behind this class is that a Sheet is a *grid* of cells.
@@ -496,74 +497,11 @@ module Office
         rowi.cells[coli].value = value
       end
     end
-  end
 
-  class SheetData
-    attr_reader :node
-    attr_reader :sheet
-    attr_reader :workbook
-
-    def initialize(node, sheet, workbook)
-      @node = node
-      @sheet = sheet
-      @workbook = workbook
-    end
-
-    def invalidate
-      @rows = nil
-    end
-
-    def rows
-      @rows ||= begin
-        node.xpath("#{Package.xpath_ns_prefix(node)}:row").map{|r| Row.new(r, workbook.shared_strings, workbook.styles) }
       end
     end
 
-    # add data to xml doc, and to the rows collection
-    def add_row(data)
-      row_node = Row.create_node(@node.document, rows.length + 1, data, workbook.shared_strings, styles: workbook.styles)
-      @node.add_child(row_node)
-      rows << Row.new(row_node, workbook.shared_strings, workbook.styles)
-    end
-
-    def to_csv(separator)
-      data = []
-      column_count = 0
-
-      rows.each do |r|
-        # pad for missing rows where r.number is discontinguous from last r.number
-        data.push([]) until data.length > r.number
-        # assign this row
-        data[r.number] = r.to_ary
-        # update column count
-        column_count = [column_count, data[r.number].length].max
       end
-
-      # pad all columns to equal length based on column count
-      data.each { |d| d.push("") until d.length == column_count }
-
-      csv = ""
-      data.each do |d|
-        # quote items containing separator
-        items = d.map { |i| i.index(separator).nil? ? i : "'#{i}'" }
-        csv << items.join(separator) << "\n"
-      end
-      csv
-    end
-
-    def debug_dump
-      data = []
-      column_count = 1
-      rows.each do |r|
-        data.push([]) until data.length > r.number
-        data[r.number] = r.to_ary.insert(0, (r.number + 1).to_s)
-        column_count = [column_count, data[r.number].length].max
-      end
-
-      headers = [ "" ]
-      0.upto(column_count - 2) { |i| headers << Cell.column_name(i) }
-
-      Logger.debug_dump_table("Excel Sheet #{@sheet.worksheet_part.name}", headers, data)
     end
   end
 end
