mwelham/office_docs

commit d5aa6f7881a010864db9acba56665be72b1887ab

Looping over paragraphs now including more than just paragraph nodes. Separated for loop tests into different files.

lib/office/word/template.rb
@@ -63,25 +63,25 @@ module Word
         paragraph_sets << footer.paragraphs
       end
 
-      paragraph_sets.each do |paragraphs|
-        # pre-render
-        # evaluate if statements
-          #TODO LATER
-        # expand for loops
-        expand_for_loops(paragraphs, data, options)
+      containers = [main_doc, main_doc.headers, main_doc.footers].flatten
+      containers.each do |container|
+
+        #TODO - IF_ELSE
+        expand_for_loops(container, data, options)
         unless options[:do_not_render] == true
-          render_section(paragraphs, data, options)
+          render_section(container, data, options)
         end
       end
     end
 
-    def expand_for_loops(paragraphs, data, options = {})
-      expander = Word::ForLoopExpander.new(main_doc, paragraphs, data, options)
-      expander.expand_for_loops
+    def expand_for_loops(container, data, options = {})
+      paragraphs = container.paragraphs
+      expander = Word::ForLoopExpander.new(main_doc, data, options)
+      expander.expand_for_loops(container)
     end
 
-    def render_section(paragraphs, data, options = {})
-      paragraphs.each_with_index do |paragraph, paragraph_index|
+    def render_section(container, data, options = {})
+      container.paragraphs.each_with_index do |paragraph, paragraph_index|
         Word::PlaceholderFinder.loop_through_placeholders_in_paragraph(paragraph, paragraph_index) do |placeholder|
           replacer = Word::PlaceholderReplacer.new(placeholder, word_document)
           replacement = replacer.replace_in_paragraph(paragraph, data, options)

lib/office/word/for_loop_expander.rb
@@ -4,28 +4,32 @@ require 'office/word/for_loop_expanders/loop_table_row'
 
 module Word
   class ForLoopExpander
-    attr_accessor :main_doc, :paragraphs, :data, :options, :placeholders
-    def initialize(main_doc, paragraphs, data, options = {})
+    attr_accessor :main_doc, :data, :options, :placeholders
+    def initialize(main_doc, data, options = {})
       self.main_doc = main_doc
-      self.paragraphs = paragraphs
       self.data = data
       self.options = options
     end
 
-    def expand_for_loops
+    def expand_for_loops(container)
       # Get placeholders in paragraphs
+      paragraphs = container.paragraphs
       self.placeholders = Word::PlaceholderFinder.get_placeholders(paragraphs)
-      i = 0
-      while i < placeholders.length
-        start_placeholder = placeholders[i]
-        if start_placeholder[:placeholder_text].include?("foreach")
-          end_index = get_end_index(i)
-          expand_loop(i, end_index)
+      while there_are_for_loop_placeholders(placeholders)
+        i = 0
+        while i < placeholders.length
+          start_placeholder = placeholders[i]
+          if start_placeholder[:placeholder_text].include?("foreach")
+            end_index = get_end_index(i)
+            expand_loop(i, end_index)
 
-          i = end_index + 1
-        else
-          i += 1
+            i = end_index + 1
+          else
+            i += 1
+          end
         end
+        paragraphs = resync_container(container)
+        self.placeholders = Word::PlaceholderFinder.get_placeholders(paragraphs)
       end
 
       # i = 0
@@ -96,6 +100,15 @@ module Word
       # First one is in a table cell - just need to make sure end is a tc and in the same row
       end_placeholder_parent.name != 'tc' ||
       start_placeholder_parent.parent != end_placeholder_parent.parent
+    end
+
+    def resync_container(container)
+      container.parse_paragraphs(container.container_node)
+      paragraphs = container.paragraphs
+    end
+
+    def there_are_for_loop_placeholders(placeholders)
+      placeholders.any?{|p| p[:placeholder_text].include?("foreach") }
     end
 
   end#endclass

lib/office/word/for_loop_expanders/loop_over_paragraphs.rb
@@ -8,38 +8,38 @@ module Word
       # loop paragraphs inbetween
 
       def expand_loop(start_placeholder, end_placeholder, inbetween_placeholders)
-        target_paragraphs = get_paragraphs(start_placeholder, end_placeholder)
-
-        container = target_paragraphs.first.document
+        container = start_placeholder[:paragraph_object].document
+        target_nodes = get_inbetween_nodes(start_placeholder, end_placeholder)
 
         #This is the 0 run of our loop.
         # Get a set of runs for the duplicate runs
-        duplicate_paragraphs = generate_new_paragraph_set(container, target_paragraphs)
+        duplicate_nodes = generate_node_set(target_nodes)
+
         for_loop_placeholder_info = parse_for_loop_placeholder(start_placeholder[:placeholder_text])
 
         field_data = for_loop_placeholder_info[:data].presence || []
         if field_data.length == 0
-          target_paragraphs.each do |paragraph|
-            container.remove_paragraph(paragraph)
-          end
+          target_nodes.each(&:remove)
         else
+          target_paragraphs = get_paragraphs_from_nodes(container, target_nodes)
           replace_variable_in_placeholders_in_paragraphs(target_paragraphs, 0, for_loop_placeholder_info, inbetween_placeholders)
-          last_paragraph = target_paragraphs.last
+          last_node = target_nodes.last
+
           field_data[1..-1].each_with_index do |data_set, i|
-            new_paragraph_set = generate_new_paragraph_set(container, duplicate_paragraphs)
+            new_node_set = generate_node_set(duplicate_nodes)
+            new_paragraph_set = get_paragraphs_from_nodes(container, new_node_set)
             replace_variable_in_placeholders_in_paragraphs(new_paragraph_set, i+1, for_loop_placeholder_info, inbetween_placeholders)
 
-            new_paragraph_set.each do |paragraph|
-              container.insert_new_paragraph_object_after_paragraph(last_paragraph, paragraph)
-              last_paragraph = paragraph
+            new_node_set.each do |node|
+              last_node.add_next_sibling(node)
+              last_node = node
             end
-            #last_paragraph = new_paragraph_set.last
+
           end
         end
-
       end
 
-      def get_paragraphs(start_placeholder, end_placeholder)
+      def get_inbetween_nodes(start_placeholder, end_placeholder)
         start_paragraph = start_placeholder[:paragraph_object]
         end_paragraph = end_placeholder[:paragraph_object]
         document = start_paragraph.document
@@ -68,22 +68,27 @@ module Word
           end_paragraph.split_after_run(end_run) if(!ends_with)
         end
 
-        start_paragraph_index = document.paragraphs.index(start_paragraph)
-        end_paragraph_index = document.paragraphs.index(end_paragraph)
-        document.paragraphs[start_paragraph_index..end_paragraph_index]
+        container = start_paragraph.node.parent
+        start_index = container.children.index(start_paragraph.node)
+        end_index = container.children.index(end_paragraph.node)
+        container.children[start_index..end_index]
       end
 
-      def generate_new_paragraph_set(container, paragraphs)
-        new_paragraph_set = []
+      def generate_node_set(nodes)
+        nodes.map(&:clone)
+      end
 
-        paragraphs.each do |p|
-          new_p = Office::Paragraph.new(p.node.clone, container)
-          new_paragraph_set << new_p
+      def get_paragraphs_from_nodes(container, nodes)
+        paragraphs = []
+        nodes.each do |node|
+          if node.name == 'p'
+            paragraphs << Office::Paragraph.new(node, container)
+          else
+            node.xpath(".//w:p").each { |p| paragraphs << Office::Paragraph.new(p, container) }
+          end
         end
-
-        new_paragraph_set
+        paragraphs
       end
-
 
     end
   end

lib/office/word/for_loop_expanders/loop_table_row.rb
@@ -35,8 +35,6 @@ module Word
             last_row = new_row
           end
         end
-
-        container.parse_paragraphs(container.container_node)
       end
 
       def get_row(start_placeholder, end_placeholder)
@@ -61,14 +59,9 @@ module Word
       def get_paragraphs_from_row(container, row)
         container_node = container
         paragraphs = []
-        row.xpath(".//w:p").each { |p| paragraphs << Office::Paragraph.new(p, self) }
+        row.xpath(".//w:p").each { |p| paragraphs << Office::Paragraph.new(p, container_node) }
         paragraphs
       end
-
-
-
-
-
 
     end
   end

test/word/for_loop_expanders/loop_in_paragraph.rb
@@ -1,0 +1,35 @@
+module LoopInParagraphTest
+  IN_SAME_PARAGRAPH_FOR_LOOP = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'in_same_paragraph_for_loop_test.docx')
+
+  #SAME PARAGRAPH LOOPS - RUN LOOPS
+  #
+  #
+  #
+  def test_loop_in_same_paragraph
+    file = File.new('test_save_simple_doc.docx', 'w')
+    file.close
+    filename = file.path
+
+    doc = Office::WordDocument.new(IN_SAME_PARAGRAPH_FOR_LOOP)
+    template = Word::Template.new(doc)
+    template.render(
+      {'fields' =>
+        {'Group' => [
+          {'Q' => 'a'},
+          {'Q' => 'b'},
+          {'Q' => 'c'}
+        ]
+      }
+    }, {do_not_render: true})
+    template.word_document.save(filename)
+
+    assert File.file?(filename)
+    assert File.stat(filename).size > 0
+
+    correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'in_same_paragraph_for_loop_test.docx'))
+    our_render = Office::WordDocument.new(filename)
+    assert docs_are_equivalent?(correct, our_render)
+
+    File.delete(filename)
+  end
+end

test/word/for_loop_expanders/loop_over_paragraphs.rb
@@ -1,0 +1,88 @@
+module LoopOverParagraphsTest
+  IN_DIFFERENT_PARAGRAPH_FOR_LOOP = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'in_different_paragraph_for_loop_test.docx')
+  IN_DIFFERENT_PARAGRAPH_FOR_LOOP_COMPLEX = File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'in_different_paragraph_complex_loop_test.docx')
+
+  #DIFFERENT PARAGRAPH LOOPS
+  #
+  #
+  #
+  def test_loop_in_different_paragraph_with_blank_group
+    file = File.new('test_loop_in_different_paragraph_with_blank_group.docx', 'w')
+    file.close
+    filename = file.path
+
+    doc = Office::WordDocument.new(IN_DIFFERENT_PARAGRAPH_FOR_LOOP)
+    template = Word::Template.new(doc)
+    template.render(
+      {'fields' =>
+        {'Group' => []
+      }
+    }, {do_not_render: true})
+    template.word_document.save(filename)
+
+    assert File.file?(filename)
+    assert File.stat(filename).size > 0
+
+    correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'in_different_paragraph_blank_group.docx'))
+    our_render = Office::WordDocument.new(filename)
+    assert docs_are_equivalent?(correct, our_render)
+
+    File.delete(filename)
+  end
+
+  def test_loop_in_different_paragraph
+    file = File.new('test_loop_in_different_paragraph.docx', 'w')
+    file.close
+    filename = file.path
+
+    doc = Office::WordDocument.new(IN_DIFFERENT_PARAGRAPH_FOR_LOOP)
+    template = Word::Template.new(doc)
+    template.render(
+      {'fields' =>
+        {'Group' => [
+          {'Q' => 'a'},
+          {'Q' => 'b'},
+          {'Q' => 'c'}
+        ]
+      }
+    }, {do_not_render: true})
+    template.word_document.save(filename)
+
+    assert File.file?(filename)
+    assert File.stat(filename).size > 0
+
+    correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'in_different_paragraphs.docx'))
+    our_render = Office::WordDocument.new(filename)
+    assert docs_are_equivalent?(correct, our_render)
+
+    File.delete(filename)
+  end
+
+  def test_complex_loop_in_different_paragraph
+    file = File.new('test_complex_loop_in_different_paragraph.docx', 'w')
+    file.close
+    filename = file.path
+
+    doc = Office::WordDocument.new(IN_DIFFERENT_PARAGRAPH_FOR_LOOP_COMPLEX)
+    template = Word::Template.new(doc)
+    template.render(
+      {'fields' =>
+        {'Group' => [
+          {'Q' => 'a'},
+          {'Q' => 'b'},
+          {'Q' => 'c'}
+        ]
+      }
+    }, {do_not_render: true})
+    template.word_document.save(filename)
+
+    assert File.file?(filename)
+    assert File.stat(filename).size > 0
+
+    correct = Office::WordDocument.new(File.join(File.dirname(__FILE__), '..', '..', 'content', 'template', 'for_loops', 'correct_render', 'test_complex_loop_in_different_paragraph.docx'))
+    our_render = Office::WordDocument.new(filename)
+    assert docs_are_equivalent?(correct, our_render)
+
+    File.delete(filename)
+  end
+end

commit 8b5bb3a606a4a3906abbd83748462095db0bfe38

Remove dead word code. Does nothing, removal breaks no tests.
seems to be fallout from

commit d5aa6f7881a010864db9acba56665be72b1887ab
Date:   Fri Nov 27 12:09:59 2015 +0200

  Looping over paragraphs now including more than just paragraph nodes. Separated for loop tests into different files.

lib/office/word/template.rb
@@ -54,15 +54,6 @@ module Word
     #
 
     def render(data, options = {})
-      # TODO this doesn't seem to do anything?
-      paragraph_sets = [main_doc.paragraphs]
-      main_doc.headers.each do |header|
-        paragraph_sets << header.paragraphs
-      end
-      main_doc.footers.each do |footer|
-        paragraph_sets << footer.paragraphs
-      end
-
       containers = [main_doc, main_doc.headers, main_doc.footers].flatten
       containers.each do |container|
         expand_for_loops(container, data, options)