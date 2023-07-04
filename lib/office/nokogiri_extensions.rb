7b3ee8cbf35a06cc554683fc8fe52fb2eec2dc62
Ashton <ashtherig@gmail.com>
2023-07-04 15:17:00 +0200

    Fix warnings in gsub regex
    warning: ambiguity between regexp and two divisions: wrap regexp in parentheses or add a space after `/' operator

--- lib/office/nokogiri_extensions.rb
+++ lib/office/nokogiri_extensions.rb
@@ -26,12 +26,12 @@ module Nokogiri::XML::Searchable
   # probably need some kind of fancy splatting and args extraction here to
   # fit in with the Searchable#xpath and Searchable#search
   def nspath xpath
-    self.xpath xpath.gsub /~(\w+)/, "node()[local-name() = '\\1']"
+    self.xpath xpath.gsub(/~(\w+)/ , "node()[local-name() = '\\1']")
   end
 
   # implement xpath-2.0 //*:tag_name syntax meaning "any namespace with local-name() = 'tag_name'"
   def nxpath xpath
-    self.xpath xpath.gsub /\*:(\w+)/, "node()[local-name() = '\\1']"
+    self.xpath xpath.gsub(/\*:(\w+)/ , "node()[local-name() = '\\1']")
   end
 end
 
