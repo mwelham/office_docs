5ea406950ff749ff22e54a24cbc121aa823686ca
Javar Alexander <javar.alexander@gmail.com>
2024-01-10 15:09:28 +0200

    chore: updates to placeholder parsing

--- lib/office/word/placeholder_finder_v2.rb
+++ lib/office/word/placeholder_finder_v2.rb
@@ -12,6 +12,7 @@ module Word
             end_time = Time.now
             total_time = end_time - start_time
             puts "Total time to get placeholders: #{total_time}"
+            byebug
             placeholders
             rescue => e
                 byebug
@@ -24,26 +25,32 @@ module Word
           runs = paragraph.runs
           run_texts = runs.map(&:text).dup
 
-          #TODO: Should this be nil, or do we need to return an object of some sort?
           return [] if run_texts.empty? || run_texts.nil?
               text = run_texts.join('')
-              text.scan(/(\{\{[^}]*\}\}|\{%[^%]*%\}|\{ %[^%]*%\}|\{%[^}]*\}\})/) do |match|
+            
+              matches = text.scan(/(\{\{[^}]*\}\}|\{%[^%]*%\}|\{\s*%[^%]*%\}|\{%[^}]*\}\}|{%\s*if[^%]*%\}|\{%\s*endif\s*%\}|\{%\s*for[^%]*%\}|\{%\s*endfor\s*%\})/) 
+                
+                text.scan(/(\{\{[^}]*\}\}|\{%[^%]*%\}|\{\s*%[^%]*%\}|\{%[^}]*\}\}|{%\s*if[^%]*%\}|\{%\s*endif\s*%\}|\{%\s*for[^%]*%\}|\{%\s*endfor\s*%\})/)  do |match|
                   placeholder_text = match[0]
                   start_position = Regexp.last_match.begin(0)
                   end_position = Regexp.last_match.end(0) - 1
-          
-                    placeholders << {
-                      placeholder_text: placeholder_text,
-                      paragraph_object: paragraph,
-                      paragraph_index: paragraph_index,
-                      beginning_of_placeholder: {
-                        run_index: calculate_run_index(run_texts, start_position),
-                        char_index: calculate_char_index(run_texts, placeholder_text)
-                      },
-                      end_of_placeholder: {
-                        run_index: calculate_run_index(run_texts, end_position),
-                        char_index: run_texts.last.to_s.length > 0 ? run_texts.last.to_s.length - 1 : 0
-                      }
+                  end_char = text[end_position]
+                  start_char = text[start_position]
+                  last_placeholder = nil
+                  last_placeholder = placeholders.last if matches.length > 1 && matches.index(match) > 0 
+                
+                  placeholders << {
+                    placeholder_text: placeholder_text,
+                    paragraph_object: {},
+                    paragraph_index: paragraph_index,
+                    beginning_of_placeholder: {
+                      run_index: calculate_run_index(run_texts, start_position),
+                      char_index: calculate_beginning_char_index(last_placeholder, run_texts, start_char)
+                    },
+                    end_of_placeholder: {
+                      run_index: calculate_run_index(run_texts, end_position),
+                      char_index: calculate_ending_char_index(last_placeholder, run_texts, end_char)
+                    }
                   }
               end
             return placeholders
@@ -51,11 +58,45 @@ module Word
 
           private
 
-          def calculate_char_index(run_texts, placeholder_text)
-            joined_text = run_texts.join(' ')
-            match_data = joined_text.match(/#{Regexp.escape(placeholder_text)}/)
-            return match_data.begin(0) if match_data
-            0 # Return 0 if the placeholder text isn't found
+          def calculate_beginning_char_index(last_placeholder, run_texts, start_char)
+
+            current_char_index = nil
+
+            if (last_placeholder.nil?)
+              ## TODO: include is nil, so we need to figure out why it's nil or add a nil check. Most likely
+              ## will need to do the same for below. 
+              found_index = run_texts.index { |str| str.include?(start_char) }
+              return found_index || 0
+            end
+
+
+            run_texts.each_with_index do |placeholder_parts, index|
+              next unless placeholder_parts.include?(start_char) && index > last_placeholder[:beginning_of_placeholder][:char_index]
+              current_char_index = index
+              break 
+            end
+
+            current_char_index || 0
+
+          end
+
+          def calculate_ending_char_index(last_placeholder, run_texts, end_char)
+            current_char_index = 0
+
+            if (last_placeholder.nil?)
+              found_index =  run_texts.index{ |str| str.include?(end_char) } 
+              
+              return found_index || 0
+            end
+
+            run_texts.each_with_index do |placeholder_parts, index|
+              next unless placeholder_parts.include?(end_char) && index > last_placeholder[:end_of_placeholder][:char_index]
+              current_char_index = index
+              break 
+            end
+
+            current_char_index
+      
           end
           
 
